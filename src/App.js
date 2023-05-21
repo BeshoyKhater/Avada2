a58e80cbd101e6ad076e3b7d1021398b8c27a62f
BeshoyKhater <BeshoyKhater@gmail.com>
2023-05-22 00:54:21 +0300

    demo

--- src/App.js
+++ src/App.js
@@ -1,16 +1,16 @@
 import Home from './pages/Home';
 import About from './pages/About';
 import Nav from './componends/Nav';
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { HashRouter, Routes, Route } from "react-router-dom";
 function App() {
   return (
-<BrowserRouter>
+<HashRouter>
  <Routes> 
 <Route path="/" element={<Nav/>}> 
 <Route index element={<Home />} />
  <Route path="about" element={<About/>} /> 
 </Route> </Routes> 
-</BrowserRouter>
+</HashRouter>
   )
 }
 
